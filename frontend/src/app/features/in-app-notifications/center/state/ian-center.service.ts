be75a884e8b95105271845c244434d8ad8d9af71
Oliver Günther <mail@oliverguenther.de>
2024-06-13 10:24:27 +0200

    Reload menu frame when marking notification as read

--- frontend/src/app/features/in-app-notifications/center/state/ian-center.service.ts
+++ frontend/src/app/features/in-app-notifications/center/state/ian-center.service.ts
@@ -71,6 +71,7 @@ import { UntilDestroyedMixin } from 'core-app/shared/helpers/angular/until-destr
 import idFromLink from 'core-app/features/hal/helpers/id-from-link';
 import { DeviceService } from 'core-app/core/browser/device.service';
 import { ApiV3ListFilter, ApiV3ListParameters } from 'core-app/core/apiv3/paths/apiv3-list-resource.interface';
+import { FrameElement } from '@hotwired/turbo';
 
 export interface INotificationPageQueryParameters {
   filter?:string|null;
@@ -91,6 +92,8 @@ export class IanCenterService extends UntilDestroyedMixin {
   notLoaded$ = this.query.select('notLoaded');
 
   activeCollection$ = this.query.select('activeCollection');
+
+  menuFrame = document.getElementById('notifications_sidemenu') as FrameElement;
 
   // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
   activeReason$:Observable<string|null> = this.uiRouterGlobals.params$!.pipe(
@@ -364,6 +367,9 @@ export class IanCenterService extends UntilDestroyedMixin {
     if (!this.deviceService.isMobile && this.state.includes('**.details.*')) {
       this.showNextNotification();
     }
+
+    // Reload the sidemenu frame
+    this.menuFrame.reload();
   }
 
   private sideLoadInvolvedWorkPackages(elements:INotification[]):Promise<unknown> {
